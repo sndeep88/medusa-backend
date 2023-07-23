7c7b431eb14b36f829926279e0417fc6c6687df2
Hassan Chaudhary <hassanfzl11@gmail.com>
2023-07-23 18:40:00 +0500

    Updated Admin panel

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || "postgres://hassan:hassan@localhost:5432/hassan";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -43,13 +43,13 @@ const plugins = [
     },
   },
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
-  // {
-  //   resolve: "@medusajs/admin",
-  //   /** @type {import('@medusajs/admin').PluginOptions} */
-  //   options: {
-  //     autoRebuild: true,
-  //   },
-  // },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+    },
+  },
 ];
 
 const modules = {
